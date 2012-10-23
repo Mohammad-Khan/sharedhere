bf10ebe17cd8f99259cb24fc09c3424da32ad5a3
Lars Lindgren <chrono@eeky.net>
2012-10-23 15:44:15 -0500

    * Playing around with Google Maps API

--- client/android/main/src/com/sharedhere/SharedHereAdapter.java
+++ client/android/main/src/com/sharedhere/SharedHereAdapter.java
@@ -1,15 +1,65 @@
 package com.sharedhere;
 
-import android.app.Activity;
-import android.os.Bundle;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
 
-public class SharedHereAdapter extends Activity
+import com.sharedhere.R;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SharedHereAdapter extends MapActivity
 {
+	private MapView mapView = null;
+	private MapController mapController = null;
+	
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        initMapView();       
+     }
+    
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	/**
+	 * 
+	 */
+	private void initMapView() {
+		if (mapView == null) {
+			mapView = (MapView) findViewById(R.id.mapview);
+			mapView.setBuiltInZoomControls(true);
+		}
+		
+		if (mapController == null) {
+			mapController = mapView.getController();
+			//mapController.setZoom(15);
+			//mapController.setCenter(new GeoPoint(10000,10000));
+		}
+	}
+	
+	/**
+     * 
+     * @param view
+     */
+    public void onClickRefresh(final View view) {
+    	updateView();
+    }
+    
+    /**
+     * 
+     */
+    public void updateView() {
+        final TextView valueView = (TextView) findViewById(R.id.textview_gps);
+		valueView.setText("hello");    
     }
 }
