20e3f5c62166d37e60efffba398fc2015ad818cb
Lars Lindgren <chrono@eeky.net>
2012-11-14 21:07:38 -0600

    * Deleted unused class LatLonPoint (functionality has already been moved to SHLocation)

--- client/android/main/src/com/sharedhere/model/LatLonPoint.java
+++
@@ -1,9 +0,0 @@
-package com.sharedhere.model;
-
-import com.google.android.maps.GeoPoint;
-
-public final class LatLonPoint extends GeoPoint {
-    public LatLonPoint(double latitude, double longitude) {
-        super((int) (latitude * 1E6), (int) (longitude * 1E6));
-    }
-}
