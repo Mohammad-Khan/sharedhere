daa411ce0520dcb65aca0f0e63996bf566ee0dd4
Santosh Gajul <santoshkumargajul@gmail.com>
2012-11-15 20:26:55 -0600

    Added the Uploaded Activity - client Side Code

--- client/android/main/src/com/sharedhere/UploadActivity.java
+++ client/android/main/src/com/sharedhere/UploadActivity.java
@@ -1,14 +1,124 @@
 package com.sharedhere;
 
-import android.app.Activity;
+
+import java.io.File;
+
+
+import com.sharedhere.model.SHArrayAdapter;
+
+import com.sharedhere.model.SHClientServer;
+
+import com.sharedhere.model.SHLocation;
+
+
+import android.app.ListActivity;
+
 import android.os.Bundle;
 
-public class UploadActivity extends Activity {
+import android.view.View;
+
+import android.widget.ListView;
+
+import android.widget.Toast;
+
+
+public class UploadActivity extends ListActivity
+ {
+	
+	private File currentDirectory = null;
+
+	private File parentDirectory = null;
+
+	private SHLocation currentLocation = null;
+
+
+	static final String ROOT_DIRECTORY = "/mnt/sdcard";
+
 
 	/** Called when the activity is first created. */
+
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+
+	public void onCreate(Bundle savedInstanceState)
+	 {
+		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.upload);
+
+		// Set current and parent directories to root (initially)
+
+		currentDirectory = new File(ROOT_DIRECTORY);
+
+		parentDirectory = currentDirectory;
+
+
+		// Set current location
+
+		currentLocation = (SHLocation)getIntent().getSerializableExtra("SHLocation");
+
+		// Process the root directory
+
+		processDirectory();
+
 	}
+
+	
+	@Override
+
+	protected void onListItemClick(ListView l, View v, int position, long id)
+	 {
+
+		File file = (File) getListAdapter().getItem(position);
+
+		if (file.isDirectory())
+		 {
+	
+			currentDirectory = file;
+
+			parentDirectory = file.getParentFile()==null?
+
+			new File(ROOT_DIRECTORY) : file.getParentFile();
+
+			
+processDirectory();
+
+		} 
+		else
+		 {
+		
+	SHClientServer shConn = new SHClientServer("http://sharedhere.zapto.org/");
+
+			// TODO CleintServer.download should return/throw boolean/exception and the return should be checked and the toast below be set accordingly
+
+			shConn.upload(file.getAbsolutePath(), currentLocation, "Testing file Naser");
+
+			Toast.makeText(this, "File uploaded", Toast.LENGTH_LONG).show();
+
+		}
+
+	}
+
+	
+/**
+
+	 * Processes the current directory
+
+	 * Assigns the content of current directory to the upload array adapter 
+
+	 * 
+ */
+	
+	private void processDirectory() 
+	{
+	
+		File[] files = currentDirectory.listFiles();
+
+		
+SHArrayAdapter adapter = new SHArrayAdapter(this, files);
+
+		setListAdapter(adapter);
+
+	}
+
+
 }
+
---
+++ client/android/main/src/com/sharedhere/model/SHArrayAdapter.java
@@ -0,0 +1,44 @@
+package com.sharedhere.model;
+
+import java.io.File;
+
+import com.sharedhere.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SHArrayAdapter extends ArrayAdapter<File> {
+  private final Context context;
+  private final File[] files;
+
+  public SHArrayAdapter(Context context, File[] files) {
+    super(context, R.layout.upload, files);
+    this.context = context;
+    this.files = files;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    LayoutInflater inflater = (LayoutInflater) context
+        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    View rowView = inflater.inflate(R.layout.upload, parent, false);
+    TextView textView = (TextView) rowView.findViewById(R.id.label);
+    ImageView imageView = (ImageView) rowView.findViewById(R.id.icon_file);
+    textView.setText(files[position].getName());
+    // Get the current file
+    File f = files[position];
+    // Select the right icon based on whether f is a folder or a regular
+    if (f.isDirectory()) {
+      imageView.setImageResource(R.drawable.folder);
+    } else {
+      imageView.setImageResource(R.drawable.file);
+    }
+
+    return rowView;
+  }
+}
